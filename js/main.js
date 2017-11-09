f5062f4b6d40eb7ac13e04fde853a4e3b210dca0
hrayr <iamhrayr@gmail.com>
2017-11-10 01:11:56 +0400

    st

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// var controller = new ScrollMagic.Controller();
+
+// $(function () {
+// 	// build scene
+// 	var scene = new ScrollMagic.Scene()
+// 		.setPin("#header")
+// 		.addIndicators({name: "1 (duration: 300)"}) // add indicators (requires plugin)
+// 		.addTo(controller);
+// });
